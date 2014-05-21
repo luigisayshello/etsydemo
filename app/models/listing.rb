3f32ee5483f70a20a2feee5aa801ae5e4f52ae42
luigisayshello <carloslemosloureiro@gmail.com>
2014-05-21 14:45:26 +0100

    try 4

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,16 +1,13 @@
 class Listing < ActiveRecord::Base
   if Rails.env.development?
-     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "no-available-image.png"
-     validates_attachment_content_type :image, :content_type => [ "image/jpg", "image/jpeg", "image/png", "image/gif"]
-
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "no-available-image.png"
   else
-     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "no-available-image",
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "no-available-image.png",
         :storage => :dropbox,
         :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
         :path => ":style/:id_:filename"
-        validates_attachment_content_type :image, :content_type => [ "image/jpg", "image/jpeg", "image/png", "image/gif"]
   end
- 
+   do_not_validate_attachment_file_type :image
 end
 
 #validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
